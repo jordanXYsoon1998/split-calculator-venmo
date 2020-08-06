e4c9d414fd300e1f1da17c6409c48aaa34ce5910
Jordan Soon <jordansoon@ucla.edu>
2020-08-05 18:48:03 -0700

    Cookie parser

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 require('./db/mongoose');
 const userRouter = require('./routers/user');
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(userRouter);
 app.get('/', (req, res) => {
   res.send('Welcome to server for SplitBill Venmo API!');
