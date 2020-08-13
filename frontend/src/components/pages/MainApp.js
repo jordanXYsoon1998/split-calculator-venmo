b5499e7987c2101279db1037f82d5168007fb9db
Jordan Soon <jordansoon@ucla.edu>
2020-08-12 19:16:15 -0700

    Test main-app use state decide redirect

--- frontend/src/components/pages/MainApp.js
+++ frontend/src/components/pages/MainApp.js
@@ -1,13 +1,50 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const MainApp = () => {
-  return (
-    <React.Fragment>
-      <h2>Welcome to the Actual App page!</h2>
-      <Link to="/" className="ui button">Return to Landing Page</Link>
-    </React.Fragment>
-  );
+import { fetchFriends, fetchPaymentMethods } from '../../actions';
+
+class MainApp extends React.Component {
+  componentDidMount() {
+    this.props.fetchFriends();
+  }
+
+  renderLogin() {
+    if (this.props.userAuth === null || this.props.venmoAuth === null) {
+      return (
+        <h3>Fetching from server. Please wait...</h3>
+      );
+    } else if (this.props.userAuth && this.props.venmoAuth) {
+      return (
+        <h3>Welcome! You are authenticated in SplitBill and Venmo!</h3>
+      );
+    } else if (!this.props.userAuth) {
+      return (
+        <h3>We need to redirect you to the user login page!</h3>
+      );
+    } else if (!this.props.venmoAuth) {
+      return (
+        <h3>We need to redirect you to the Venmo login page!</h3>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        {this.renderLogin()}
+        <h2>Welcome to the Actual App page!</h2>
+        <Link to="/" className="ui button">Return to Landing Page</Link>
+      </React.Fragment>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { userAuth: state.userAuth, venmoAuth: state.venmoAuth };
 };
 
-export default MainApp;
+export default connect(
+  mapStateToProps,
+  { fetchFriends, fetchPaymentMethods }
+)(MainApp);
