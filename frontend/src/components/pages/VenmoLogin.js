df2e6a131b78d77f59ba6933881ec5c84742b5d7
Jordan Soon <jordansoon@ucla.edu>
2020-08-16 20:49:13 -0700

    Venmo Login intro and UserCredentials piece adjustment

--- frontend/src/components/pages/VenmoLogin.js
+++ frontend/src/components/pages/VenmoLogin.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import history from '../../history';
+import splitbill from '../../apis/splitbill';
+import { } from '../../actions';
+import UserCredentials from '../pieces/UserCredentials';
 
 const VenmoLogin = () => {
+  const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState([]);
+  const dispatch = useDispatch();
+
+  // TODO: Work on the Venmo and decide OTP
+  const onFormSubmit = async ({ email, password }) => {
+    setErrors([]);
+    setLoading(true);
+    try {
+      await splitbill.post('/venmoUsers/login', { email, password });
+      // dispatch(venmoLogin());
+      history.push('/main-app');
+    } catch (err) {
+      setLoading(false);
+      if (err.response.status === 401) {
+        setErrors([...errors, { message: 'Incorrect email or password' }]);
+      }
+    }
+  };
+
+  const venmoLogo = {
+    src: 'https://cdn1.venmo.com/marketing/images/branding/venmo-icon.svg',
+    alt: 'Venmo Branded Logo'
+  };
+
   return (
-    <div>
-      <h2>Welcome to the Venmo Login Page!</h2>
-    </div>
+    <UserCredentials
+      pageTitle="Login to Venmo"
+      onFormSubmit={onFormSubmit}
+      loadingState={loading}
+      errors={errors}
+      logo={venmoLogo}
+    />
   );
 };
 
--- frontend/src/components/pieces/UserCredentials.js
+++ frontend/src/components/pieces/UserCredentials.js
@@ -2,13 +2,44 @@ import React, { useState } from 'react';
 import Field from './Field';
 import './UserCredentials.css';
 
-const UserCredentials = ({ pageTitle, onFormSubmit, loadingState, errors, children } = {}) => {
+const UserCredentials = ({
+  pageTitle,
+  onFormSubmit,
+  loadingState,
+  logo,
+  errors,
+  children
+} = {}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
     onFormSubmit({ email, password });
+  };
+
+  const renderHeader = () => {
+    if (logo) {
+      return (
+        <h2 className="ui image header">
+          <img
+            src={logo.src}
+            alt={logo.alt}
+            className="image"
+          />
+          <div className="content">
+            {pageTitle}
+          </div>
+        </h2>
+      );
+    }
+    return (
+      <h2 className="ui header">
+        <div className="content">
+          {pageTitle}
+        </div>
+      </h2>
+    );
   };
 
   const renderErrors = () => {
@@ -30,15 +61,21 @@ const UserCredentials = ({ pageTitle, onFormSubmit, loadingState, errors, childr
     return null;
   };
 
+  const renderChildren = () => {
+    if (children) {
+      return (
+        <div className="ui message">
+          {children}
+        </div>
+      );
+    }
+  };
+
   return (
     <div className="grid-container">
       <div className="ui middle aligned center aligned grid">
         <div className="column">
-          <h2 className="ui header">
-            <div className="content">
-              {pageTitle}
-            </div>
-          </h2>
+          {renderHeader()}
           <form
             className={`ui large ${loadingState ? 'loading ' : ''}form error`}
             onSubmit={handleSubmit}
@@ -69,9 +106,7 @@ const UserCredentials = ({ pageTitle, onFormSubmit, loadingState, errors, childr
               {renderErrors()}
             </div>
           </form>
-          <div className="ui message">
-            {children}
-          </div>
+          {renderChildren()}
         </div>
       </div>
     </div>
