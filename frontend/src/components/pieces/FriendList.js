bd4dbe7fc8a4b874ec0abfb0a87fb4bfec375776
Jordan Soon <jordansoon@ucla.edu>
2020-08-28 17:33:03 -0700

    Search functionality implemented

--- frontend/src/components/pieces/FriendList.js
+++ frontend/src/components/pieces/FriendList.js
@@ -6,8 +6,24 @@ import FriendItem from './FriendItem';
 import './FriendList.css';
 
 class FriendList extends React.Component {
+  state = { searchTerm: '' };
+
   componentDidMount() {
     this.props.fetchFriends();
+  }
+
+  renderSearch() {
+    return (
+      <div className="ui icon input">
+        <input
+          type="text"
+          placeholder="Search friends..."
+          value={this.state.searchTerm}
+          onChange={(e) => this.setState({ searchTerm: e.target.value })}
+        />
+        <i className="search icon"></i>
+      </div>
+    );
   }
 
   renderFriends() {
@@ -15,13 +31,22 @@ class FriendList extends React.Component {
       return null;
     }
 
+    const lowerSearchTerm = this.state.searchTerm.toLowerCase();
+
     return this.props.venmoFriends.map((friend) => {
-      return (
-        <FriendItem
-          billSelected={friend.bill}
-          venmoObj={friend.friendObj}
-        />
-      );
+      // Only render filtered characters
+      const { username, display_name } = friend.friendObj;
+      if (username.toLowerCase().includes(lowerSearchTerm) ||
+            display_name.toLowerCase().includes(lowerSearchTerm)) {
+        return (
+          <FriendItem
+            billSelected={friend.bill}
+            venmoObj={friend.friendObj}
+            key={friend.friendObj.id}
+          />
+        );
+      }
+      return null;
     });
   }
 
@@ -31,6 +56,7 @@ class FriendList extends React.Component {
         id="friend-list"
         className="ui left fixed large borderless vertical menu"
       >
+        {this.renderSearch()}
         <div className="ui link items">
           {this.renderFriends()}
         </div>
