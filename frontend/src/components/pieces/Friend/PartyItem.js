220e1bb9aca6cbffee378fad07df82f74a06426e
Jordan Soon <jordansoon@ucla.edu>
2020-09-03 00:04:49 -0700

    Tweak PartyItem to only show delete icon on hover

--- frontend/src/components/pieces/Friend/PartyItem.js
+++ frontend/src/components/pieces/Friend/PartyItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 import { removeFriendFromBill } from '../../../actions';
@@ -6,6 +6,7 @@ import './PartyItem.css';
 
 const FriendPartyItem = ({ venmoObj }) => {
   const { profile_picture_url, display_name, username } = venmoObj || {};
+  const [hover, setHover] = useState(false);
   const dispatch = useDispatch();
 
   const onItemClick = () => {
@@ -16,13 +17,17 @@ const FriendPartyItem = ({ venmoObj }) => {
     <div className="item">
       <div
         data-tooltip={display_name}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
       >
-        <a
-          className="ui right corner tiny label"
-          onClick={onItemClick}
-        >
-          <i className="delete icon"></i>
-        </a>
+        {hover ? (
+          <a
+            className="floating ui tiny label"
+            onClick={onItemClick}
+          >
+            <i className="delete icon"></i>
+          </a>
+        ) : null}
         <img
           className="ui circular image profile-pic"
           src={profile_picture_url}
