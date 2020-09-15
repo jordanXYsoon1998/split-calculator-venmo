768d3d16e59329284fb3274654803b42cc496e65
Jordan Soon <jordansoon@ucla.edu>
2020-09-15 14:34:40 -0700

    Get rid of simulated server delay

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 // Simulate server delay
-app.use(function(req, res, next) {setTimeout(next, 1000)});
+// app.use(function(req, res, next) {setTimeout(next, 1000)});
 app.use('/api/split-bill', overallRouter);
 
 module.exports = app;
