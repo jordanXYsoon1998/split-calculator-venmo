bf8f6578439e331b2a0339b12e3c72ec8a181dfd
Jordan Soon <jordansoon@ucla.edu>
2020-09-15 16:34:25 -0700

    Setup flow to deploy whole folder to Heroku

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 require('./db/mongoose');
 const overallRouter = require('./routers/overall');
 
@@ -7,8 +8,16 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+const buildPath = path.join(__dirname, '..', 'build');
+app.use(express.static(buildPath));
 // Simulate server delay
 // app.use(function(req, res, next) {setTimeout(next, 1000)});
 app.use('/api/split-bill', overallRouter);
+
+// Allow client-side routing
+app.use('/*', function (req, res) {
+  res.sendFile(path.join(buildPath, 'index.html'));
+});
 
 module.exports = app;
